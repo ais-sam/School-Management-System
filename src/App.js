6e3415d73ac30b9573ad61f1506fa25e17788ca7
issam <aissam.16.25@gmail.com>
2023-04-30 14:47:56 +0100

    add pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom"
+import Dashboard from './pages/Dashboard';
+import New from './pages/New';
+import Login from './pages/Login';
+import List from './pages/List';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={ <Login/> } />
+        <Route path="home" >
+          <Route index element={<Dashboard/>}/>
+          <Route path="students" >
+            <Route index element={<List/>}/>
+            <Route path='new' element={<New/>}/>
+          </Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
