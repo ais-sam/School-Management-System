b721415067206eefc31283b51800a7573d80e507
issam <aissam.16.25@gmail.com>
2023-05-04 15:45:14 +0100

    Add pageTitle component

---
+++ src/components/global/PageTitle.js
@@ -0,0 +1,9 @@
+
+
+const PageTitle = ({title}) => {
+  return (
+    <div className='title relative text-left text-xl font-bold capitalize mb-4'><span>{title}</span></div>
+  )
+}
+
+export default PageTitle
