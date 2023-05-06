93252df0c8ede6e9c4e76218e0e7afc76e7b46cc
issam <aissam.16.25@gmail.com>
2023-05-06 18:43:50 +0100

    Add Buttons component

---
+++ src/components/New/Buttons.js
@@ -0,0 +1,13 @@
+import { Button } from '@tremor/react'
+import React from 'react'
+
+const Buttons = () => {
+  return (
+    <div className="mt-5 flex items-center gap-3">
+          <Button size="lg" onClick={() => console.log("clicked")} className="bg-d-red rounded-sm hover:bg-d-red border-d-red px-10">Save</Button>
+          <Button size="lg" onClick={() => console.log("clicked")} className="bg-d-blue rounded-sm hover:bg-d-blue border-d-blue px-10">Reset</Button>
+    </div>
+  )
+}
+
+export default Buttons
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState } from "react";
+import BreadCrumb from '../components/global/BreadCrumb'
+import PageTitle from '../components/global/PageTitle'
+import Input from '@mui/joy/Input';
+// import { Label, TextInput } from 'flowbite-react';
+import { TextInput } from "@tremor/react";
+import {SelectBox,SelectBoxItem, Button} from "@tremor/react";
+import StudentData from "../components/New/StudentData";
+import ParentData from "../components/New/ParentData";
+import Buttons from "../components/New/Buttons";
+import StudentImage from "../components/New/StudentImage";
+
 
 const New = ({type}) => {
   return (
-    <div className='flex-grow'>New {type}</div>
-  )
+    <div className="text-left">
+      <PageTitle title={type}  />
+      <BreadCrumb />
+
+      <div className="bg-white rounded-sm shadow-sm p-5 mt-6">
+        <form>
+          <StudentData/>
+          <ParentData/>
+          <StudentImage/>
+          <Buttons/>
+        </form>
+      </div>
+      </div>
+    
+  );
 }
 
 export default New
